ecbf986eb9b8593a9de955eb91bb44892007d848
pine0113 <pine0113@GI-Air.local>
2022-06-19 00:42:22 +0800

    slice herochar sprites & add 2d movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed;
+    private Rigidbody2D myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 PlayerVel = new Vector2(moveDir * runSpeed,myRigidbody.velocity.y);
+        myRigidbody.velocity = PlayerVel;
+
+    }
+}
+
